7972206e634f8838d3f1510db9c8eb12c1874048
MayaBenHarush <maya.benharush@post.runi.ac.il>
2023-12-22 16:44:03 +0200

    change one

--- Coins.java
+++ Coins.java
@@ -2,7 +2,7 @@ public class Coins {
 
     public static void main(String[] args) {  
         //program that gats a number of cents as a command line argument and prints how to represent this quantity using as many quarters as possible plus the remainder in  cents
-        int input = Integer.parseInt(args[0]);
+        int a = Integer.parseInt(args[0]);
         int quarters = a / 25;
         int cent = a % 25;
         System.out.println( " use " + quarters + " quarters and " + cent + " cents " ) ;
--- LinearEq.java
+++ LinearEq.java
@@ -5,7 +5,7 @@ public class LinearEq {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-        Double x= ((c-b)/a);  
+        double x = ((c - b) / a);  
         System.out.println( a + " * x + " + b + " = " + c );
         System.out.println (" x = " + x);
     }
